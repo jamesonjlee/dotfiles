70f718708df17049032a15b6673fd269f958cf77
Jameson <jamesonjlee@gmail.com>
2013-11-06 22:32:25 -0800

    [chef] stuff

--- chef/knife.rb
+++ chef/knife.rb
@@ -1,3 +1,11 @@
-cookbook_copyright 'Suckers Kawwwwwww Inc.'
-cookbook_email 'jamesonjlee@gmail.com'
-cookbook_license 'apachev2'
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "jamesonjlee"
+client_key               "#{current_dir}/jamesonjlee.pem"
+validation_client_name   "jamesonjlee-validator"
+validation_key           "#{current_dir}/jamesonjlee-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/jamesonjlee"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
